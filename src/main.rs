6e7a984849fdb4d4ae3c5ce95fccd13974589153
Taneli <taneli.leppanen@hotmail.com>
2023-07-29 20:49:32 +0300

    Add get n_le_bits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::hash::Hash;
 
+#[derive(Debug)]
 struct HyperLogLog {
-    register: Vec<u32>,
+    register: Vec<u8>,
     index_bits: u8
 }
 
@@ -15,7 +16,7 @@ impl Default for HyperLogLog {
 impl HyperLogLog {
     /// Create a new HyperLogLog(HLL) set with first `index_bits` used as register indexes
     fn new(index_bits: u8) -> Result<Self, String> {
-        if index_bits > 8 || index_bits < 1 {
+        if !(1..=8).contains(&index_bits) {
             return Err(
                 format!(
                     "Number of index bits must be more than 0 and less than 9 (was {})", index_bits
@@ -23,7 +24,7 @@ impl HyperLogLog {
             );
         }
         let m: usize = helpers::registers_from_bits(&index_bits);
-        Ok(Self { register: vec![0; m as usize], index_bits })
+        Ok(Self { register: vec![0; m], index_bits })
     }
 
     /// Count the number of registers based on used `index_bits`
@@ -52,7 +53,7 @@ mod helpers {
         let mut hasher = hash32::Murmur3Hasher::default();
         value.hash(&mut hasher);
         let hash: u32 = hasher.finish32();
-        return hash;
+        hash
     }
 
     /// Return `n` big endian (most significant) bits of a `value`
@@ -61,10 +62,16 @@ mod helpers {
         value >> shift_amount
     }
 
+    /// Return `n` least endian bits of a `value`
+    pub fn n_le_bits(value: &u32, n: &u32) -> u32 {
+        let bitmask: u32 = (1 << n) - 1;
+        value & bitmask
+    }
+
     /// Calculate number of registers based on `index_bits`
     pub fn registers_from_bits(index_bits: &u8) -> usize {
         2_usize.checked_pow(*index_bits as u32).unwrap()
-    } 
+    }
 }
 
 fn main() {
@@ -85,5 +92,12 @@ mod tests {
         let number: u32 = 0b1010_0100_0000_0000_0000_0000_0000_0000;
         let ret = helpers::n_be_bits(&number, &6);
         assert_eq!(ret, 0b101001);
+    }
+
+    #[test]
+    fn test_n_le_bits() {
+        let number: u32 = 0b1010_0100;
+        let ret = helpers::n_le_bits(&number, &3);
+        assert_eq!(ret, 0b100);
     }
 }
